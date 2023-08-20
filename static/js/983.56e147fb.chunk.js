e7eb13ef9cee10a3657f82a8aa8eaac335bc36f4
TrifonovDenys <54479637+TrifonovDenys@users.noreply.github.com>
2023-08-20 20:28:43 +0000

    Deploying to gh-pages from @ TrifonovDenys/goit-react-hw-08-phonebook@83b73d2e067bac8a8b01ebbc2b47fe4d9ec8c3ec 🚀

---
+++ static/js/983.56e147fb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[983],{3983:function(e,n,o){o.r(n);var t=o(184);n.default=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)("p",{children:"HomePage"})})}}}]);
+//# sourceMappingURL=983.56e147fb.chunk.js.map
